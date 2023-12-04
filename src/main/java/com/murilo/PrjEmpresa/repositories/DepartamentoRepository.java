6a1f0d6d29a5090557e790bfddf535c34be380f9
Murilo Zocca <133894897+MRzocca@users.noreply.github.com>
2023-12-04 12:13:18 -0300

    Add files via upload

---
+++ src/main/java/com/murilo/PrjEmpresa/repositories/DepartamentoRepository.java
@@ -0,0 +1,9 @@
+package com.murilo.PrjEmpresa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.murilo.PrjEmpresa.entities.Departamento;
+
+public interface DepartamentoRepository extends JpaRepository<Departamento, Long> {
+
+}
